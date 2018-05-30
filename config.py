ca71db5b3269a2625186b3806388961dcc7c6495
Denis T <tr.denis@gmail.com>
2018-05-30 17:04:16 +0300

    migrations

--- config.py
+++ config.py
@@ -3,6 +3,20 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+def get_secret(key):
+    """
+    gets application config params
+    TODO: rewrite to db impl
+    :param key:
+    :return:
+    """
+    from params import configs
+
+    return configs.get(key, None)
+
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, "data", 'test.db')
+    db_u = get_secret('DB_USERNAME')
+    db_p = get_secret('DB_PASS')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://{}:{}@localhost/mv_media'.format(db_u, db_p)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from app import app
+from app import app, db
+from app.model import Search
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Search': Search}
 
 if __name__ == '__main__':
     app.run(port=8001, debug=True)
