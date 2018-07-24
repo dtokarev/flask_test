ce0e7a3a1f42f3ec5ac8c5e30ad0af32167fa87f
Denis T <tr.denis@gmail.com>
2018-07-24 17:53:18 +0300

    model to dict converter

--- app_parser/utils/dict.py
+++ app_parser/utils/dict.py
@@ -5,3 +5,19 @@ def get_by_list(d: dict, l: list) -> Union[str, None]:
     for e in l:
         if e in d:
             return d.get(e)
+
+
+def model_to_dict(obj):
+    from sqlalchemy import inspect
+    from datetime import datetime, date
+    import enum
+
+    def convert(exp):
+        if isinstance(exp, (datetime, date)):
+            return exp.isoformat()
+        elif isinstance(exp, enum.Enum):
+            return exp.value
+        return exp
+
+    return {c.key: convert(getattr(obj, c.key))
+            for c in inspect(obj).mapper.column_attrs}
